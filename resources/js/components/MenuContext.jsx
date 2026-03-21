5ae0a9d1ae2ea989fd4efd8a27cfeaf6a9fb2ab0
Wasuremeno <wasuremono19@gmail.com>
2026-03-21 11:26:07 +0300

    refactor: replace fixed heights with h-* classes and simplify caret-transparent utility

--- resources/js/components/MenuContext.jsx
+++ resources/js/components/MenuContext.jsx
@@ -5,10 +5,10 @@ const MenuContext = () => {
     return (
         <div className="container mx-auto px-4 py-8 pt-10">
             <div className="dark:bg-[#212121] bg-white w-[75vw] md:w-[45vw] rounded-[18px] shadow-md dark:text-white text-black mx-auto mb-4">
-                <div className='max-w-md mx-auto h-[100px] rounded-lg p-6 '>
+                <div className='max-w-md mx-auto h-25 rounded-lg p-6 '>
                                     <Link
                     href="/" 
-                    className="text-black dark:text-white [caret-color:transparent]"
+                    className="text-black dark:text-white caret-transparent"
                 >
                     Back
                 </Link>
@@ -16,19 +16,19 @@ const MenuContext = () => {
             </div>
 
             <div className="dark:bg-[#212121] bg-white w-[75vw] md:w-[45vw] rounded-[18px] shadow-md dark:text-white text-black mx-auto mb-4">
-                    <div className='max-w-md mx-auto h-[400px] rounded-lg p-6'>
-    <Link href="/" className="block text-black dark:text-white [caret-color:transparent] py-2">Settings</Link>
-    <Link href="/" className="block text-black dark:text-white [caret-color:transparent] py-2">Share</Link>
-    <Link href="/" className="block text-black dark:text-white [caret-color:transparent] py-2">Rate</Link>
-    <Link href="/" className="block text-black dark:text-white [caret-color:transparent] py-2">Support</Link>
-    <Link href="/" className="block text-black dark:text-white [caret-color:transparent] py-2">About</Link>
+                    <div className='max-w-md mx-auto h-100 rounded-lg p-6'>
+    <Link href="/" className="block text-black dark:text-white caret-transparent py-2">Settings</Link>
+    <Link href="/" className="block text-black dark:text-white caret-transparent py-2">Share</Link>
+    <Link href="/" className="block text-black dark:text-white caret-transparent py-2">Rate</Link>
+    <Link href="/" className="block text-black dark:text-white caret-transparent py-2">Support</Link>
+    <Link href="/" className="block text-black dark:text-white caret-transparent py-2">About</Link>
                 </div>
             </div>
 
             <div className="dark:bg-[#212121] bg-white w-[75vw] md:w-[45vw] rounded-[18px] shadow-md dark:text-white text-black mx-auto">
-                <div className='max-w-md mx-auto h-[100px] rounded-lg p-6 '>
-                   <Link href="/" className="block text-black dark:text-white [caret-color:transparent]">Create backup</Link>
-                    <Link href="/" className="block text-black dark:text-white [caret-color:transparent]">Restore data</Link>  
+                <div className='max-w-md mx-auto h-25 rounded-lg p-6 '>
+                   <Link href="/" className="block text-black dark:text-white caret-transparent">Create backup</Link>
+                    <Link href="/" className="block text-black dark:text-white caret-transparent">Restore data</Link>  
                     </div> 
             </div>
         </div>
